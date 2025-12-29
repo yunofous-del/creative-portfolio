206534500c3eaad25d2cc8a0f35e4b077284c498
yunofous-del <yunofous@gmail.com>
2025-12-29 08:32:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
 
+
+hamburger.addEventListener('click', () => {
+menu.classList.toggle('active');
+});
+
+
+// close menu when link clicked
+document.querySelectorAll('.menu-overlay a').forEach(link => {
+link.addEventListener('click', () => {
+menu.classList.remove('active');
+});
+});
